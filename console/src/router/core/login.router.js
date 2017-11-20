59083099239744dc69e0cabc749bd090da923761
jesseqiu <jesse.qiu@drigle.com>
2017-11-20 17:15:30 +0800

    update

--- console/src/router/core/login.router.js
+++
@@ -1,9 +0,0 @@
-const Login = resolve => require(['@/views/login/login'], resolve);
-
-const loginRouter = [{
-  path: '/login',
-  component: Login,
-  hidden: true,
-}]
-
-export default loginRouter;
--- console/src/router/index.js
+++
@@ -1,8 +0,0 @@
-import Login from './core/login.router';
-import Dashboard from './core/dashboard.router'; 
-
-let routers = [];
-routers = routers.concat(Login);
-routers = routers.concat(Dashboard);
-
-export default routers;
