053098f50a9f24d5f0b223d05268bdd8cab49f16
jesseqiu <jesse.qiu@drigle.com>
2017-11-27 09:50:19 +0800

    update

--- server/routers/api.js
+++ server/routers/api.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 const md5 = crypto.createHash('md5');
 const resultRes = require('./../utils/response');
 const User = require('./../models/User');
-/*
+/**
  * register 用户注册
  */
 router.post('/user/register', function (req, res, next) {
@@ -35,5 +35,51 @@ router.post('/user/register', function (req, res, next) {
     });
   })
 });
+
+/**
+ * login 用户登录
+ */
+router.post('/user/login', function (req, res, next) {
+  const {
+    username,
+    password
+  } = req.body;
+  if (!username || !password) {
+    return res.json(resultRes.err(400, '缺少必要参数或参数值为空'));
+  }
+  // 检查用户名是否已经注册
+  User.findOne({
+    username: username
+  }).then(user => {
+    password = md5.update(password).digest('base64')
+    if (user) {
+      if(user.password !== password) {
+        return res.json(resultRes.err(400, '用户名或密码错误'));
+      } else {
+        // 设置token
+
+        delete user.password;
+        return res.json(resultRes.data('登录成功', user));
+      }
+    } else {
+      return res.json(resultRes.err('400', '用户名不存在'));
+    }
+  })
+});
+
+
+/**
+ * updateById 更新用户信息 
+  username: String, // 用户名
+  nickname: String, // 昵称
+  mobile : String, // 手机号
+  email : String , // 邮箱账号
+  cnname: String, // 中文名
+  usname: String, // 英文名
+ */
+router.put('/user/:id',function (req,res,next) {
+  const {username, nickname, mobile, email, cnname, usname} = req.body;
+});
+
 
 module.exports = router;
--- server/schemas/users.js
+++ server/schemas/users.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose');
 
 // 初始化数据结构模型并导出
 module.exports = new mongoose.Schema({
-  username: String,
-  password: String
+  username: String, // 用户名
+  password: String, // 密码
+  nickname: String, // 昵称
+  mobile : String, // 手机号
+  email : String , // 邮箱账号
+  cnname: String, // 中文名
+  usname: String, // 英文名
+  avatar: String, // 头像
+  gallery: String, //图集
+  content: String, // 宣传册
+  createdAt: Date, // 注册时间
+  updatedAt: Date, // 更新时间
+  status: Boolean // 状态
 });
